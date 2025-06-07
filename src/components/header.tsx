729557c39f67d252606831010e107b3cb3c88aca
yeasinmiah1272001 <137316520+yeasinmiah1272001@users.noreply.github.com>
2025-06-07 16:28:55 +0600

    project setup done

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Container from "./container";
+
+const Header = () => {
+  return (
+    <div className="bg-[#0A1D37] text-white">
+      <Container className="">
+        <h1>Header</h1>
+      </Container>
+    </div>
+  );
+};
+
+export default Header;
